dbd945344f2212ed4b4f6cb4ba39cd1b56e4fffd
ndessi <dessi.netsanet@gmail.com>
2017-07-31 23:41:42 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+// Create a variable humanScore. Give it a value of the number 0.
+var humanScore= 0;
+// Create a variable computerScore. Give it a value of the number 0.
+var computerScore= 0;
+// Create a function play. It should accept two parameters: humanPlay and computerPlay.
+function play (humanPlay,computerPlay)
+// Inside the function:
+
+
+ //console.log("rock+humenPlay+scissors+computerPlay");
+ console.log(rock+humenPlay,scissors+computerPlay);
+  }
+  play("gaby","ui");
+  // Log "You played {humanPlay}. The bot played {computerPlay}" to the console.
+  // For example: "You played rock. The bot played scissors."
+
+  // If the human play is equal to the computer play
+if (humanPlay===computerPlay){
+
+}
+    // Log "You tied. :|" to the console.
+    console.log ("You tied");
+  // Else if the humanPlay is 'paper' and the computerPlay is 'rock' OR
+} else if ((humanPlay==="paper" && computerPlay==="rock")) ||
+
+  // humanPlay is 'rock' and the computerPlay is 'scissors' OR
+  (humanPlay==="rock" && computerPlay==="scissors")||
+  // humanPlay is 'scissors' and the computerPlay is 'paper'
+  (humenPlay==="sxissors" && computerPlay==="paper"))
+}
+    // Log "You win! :)" to the console
+   console.log ("You win");
+    // Add one to the humanScore variable (be sure to use += )
+  } humenScore= humanScore+=
+  // Otherwise log "You lose! :(" to the console
+  else {
+    console.log ("You Lose");
+
+    // Add one to the computerScore variable (be sure to use += )
+computerScore= computerScore +=;
+}
+  // Log "You have {humanScore} points" to the console
+  console.log ("You have" + humanScore +="point")
+  // For example: "You have 0 points"
+  // Log "The bot has {computerScore} points" to the console.
+  console.log ("The bot has" + computerScore + "points")
+}
+  // For example: "The bot has 3 points"
+
+
+// After defining the function, call the function 5 - 6 times,
+// Providing a different play for the computer and human each time.
+// Hint: You'll need to use two arguments
+// each time you call the play function.
+
+
+// BONUS:
+// Create a "best of 3" version
+
+// SUPER CHALLENGE:
+// Google and implement a random choice for the computer each time.
